380bca02ecbfae03ec617340dad0a1db7e50a5b8
Allen <rtallen226@gamil.com>
2022-08-20 11:51:02 +0800

    新增資料庫

---
+++ DatabaseCRUD/DatabaseCRUD/Models/NorthWindContext.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace DatabaseCRUD.Models
+{
+    public partial class NorthWindContext : DbContext
+    {
+        public NorthWindContext()
+        {
+        }
+
+        public NorthWindContext(DbContextOptions<NorthWindContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<MyProduct> MyProducts { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                IConfigurationRoot Configuration=new ConfigurationBuilder()
+                    .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                    .AddJsonFile("appsettings.json")
+                    .Build();
+                optionsBuilder.UseSqlServer(Configuration.GetConnectionString("MyDataBasr"));
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<MyProduct>(entity =>
+            {
+                entity.HasNoKey();
+
+                entity.Property(e => e.LaunchDate).HasColumnType("datetime");
+
+                entity.Property(e => e.ProductId).HasColumnName("ProductID");
+
+                entity.Property(e => e.ProductName).HasMaxLength(50);
+
+                entity.Property(e => e.UnitPrice).HasColumnType("money");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
--- DatabaseCRUD/DatabaseCRUD/Program.cs
+++ DatabaseCRUD/DatabaseCRUD/Program.cs
@@ -1,7 +1,17 @@
-var builder = WebApplication.CreateBuilder(args);
+using DatabaseCRUD.Models;
+using Microsoft.EntityFrameworkCore;
 
+var builder = WebApplication.CreateBuilder(args);
+var MyDatabasestring = builder.Configuration.GetConnectionString("MyDataBasr");
+builder.Services.AddDbContext<NorthWindContext>(opt =>
+{
+    opt.UseSqlServer(MyDatabasestring);
+});
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession();
 
 var app = builder.Build();
 
@@ -12,7 +22,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
+app.UseSession();
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
